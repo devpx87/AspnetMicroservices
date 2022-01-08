bad67ae773d6a5518ff99c26d4c3c4a395c7902e
Parixit Dave <dev.px87@gmail.com>
2022-01-08 12:30:23 +0530

    basket api

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCardItemList> Items { get; set; } = new List<ShoppingCardItemList>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
